73b49845f2176b404a92823f7f322f6c7206555a
Lucas Bighi <lucas.bighi@uzzo.io>
2020-10-26 10:26:51 -0300

    Initial Commit

---
+++ SimpleChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleChat
+//
+//  Created by Lucas Marques Bighi on 26/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
